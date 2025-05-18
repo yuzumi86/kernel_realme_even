1015297fc8c14a631d288ec32b50b516df79b313
yuzumi 86 <rullsmodsone@gmail.com>
2025-05-18 23:32:44 +0000

    arch: arm64/configs: Build wlan and bt driver inline into kernel
    Signed-off-by: yuzumi 86 <rullsmodsone@gmail.com>

--- scripts/basic/.fixdep.cmd
+++ scripts/basic/.fixdep.cmd
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 cmd_scripts/basic/fixdep := gcc -Wp,-MD,scripts/basic/.fixdep.d -Wall -Wmissing-prototypes -Wstrict-prototypes -O2 -fomit-frame-pointer -std=gnu89 -pipe      -o scripts/basic/fixdep scripts/basic/fixdep.c  
+=======
+cmd_scripts/basic/fixdep := gcc -Wp,-MD,scripts/basic/.fixdep.d -Wall -Wmissing-prototypes -Wstrict-prototypes -Wno-deprecated-declarations -O2 -fomit-frame-pointer -std=gnu89      -o scripts/basic/fixdep scripts/basic/fixdep.c  
+>>>>>>> 5093a6c56d73 (arch: arm64/configs: Build wlan and bt driver inline into kernel)
 
 source_scripts/basic/fixdep := scripts/basic/fixdep.c
 
